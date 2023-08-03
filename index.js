02dfef43f9bbc89813d5004a1298e5fa78d370bf
Deetalisman <ishmaeldotun@gmail.com>
2023-08-03 12:34:01 +1000

    toggle ham

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
-const tab = document.querySelector('.tab');
-const panel = document.querySelectorAll('.panel');
-tab.addEventListener('click', function(e){
-    if(e.target.tagName == "LI"){
-        const targetPanel = document.querySelector(e.target.dataset.target);
-        panel.forEach(function(panel){
-            if(panel == targetPanel){
-                panel.classList.add('active');
-            }else{
-                panel.classList.remove('active');
-            }
-        })
-    }
+const tab = document.querySelector(".tab");
+const panel = document.querySelectorAll(".panel");
+tab.addEventListener("click", function (e) {
+  if (e.target.tagName == "LI") {
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panel.forEach(function (panel) {
+      if (panel == targetPanel) {
+        panel.classList.add("active");
+      } else {
+        panel.classList.remove("active");
+      }
+    });
+  }
 });
 
-
-
-const ham = document.querySelector('.hamburger');
-const nav = document.querySelector('#navlist');
+const ham = document.querySelector(".hamburger");
+const nav = document.querySelector(".header ul");
+const close = document.querySelector(".close");
 ham.addEventListener("click", () => {
-    nav.classList.toggle('show')
+  nav.style.display = "block";
+  ham.style.display = "none";
+  close.style.display = "block";
+});
+close.addEventListener("click", () => {
+  nav.style.display = "none";
+  close.style.display = "none";
+  ham.style.display = "block";
 });
 
 const skill = document.querySelector("#ski");
 const exp = document.querySelector("#exp");
 const edu = document.querySelector("#edu");
-skill.addEventListener("click", function(e){
-    skill.classList.add("tabact")
-    exp.classList.remove("tabact")
-    edu.classList.remove("tabact")
-})
-edu.addEventListener("click", function(e){
-    edu.classList.toggle("tabact")
-    exp.classList.remove("tabact")
-    skill.classList.remove("tabact")
-})
-exp.addEventListener("click", function(e){
-    exp.classList.toggle("tabact")
-    skill.classList.remove("tabact")
-    edu.classList.remove("tabact")
-})
-
-const more = document.querySelector("#more")
-const less = document.querySelector("#less")
-const see = document.querySelectorAll(".see")
-more.addEventListener("click", function(e){
-    for(let se of see){
-    se.style.display = "block"
-    }
-    more.style.display = "none"
-    less.style.display = "block"
+skill.addEventListener("click", function (e) {
+  skill.classList.add("tabact");
+  exp.classList.remove("tabact");
+  edu.classList.remove("tabact");
 });
-less.addEventListener("click",function(e){
-    for(let se of see){
-        se.style.display = "none"
-        }
-        more.style.display = "block"
-        less.style.display = "none"
-})
+edu.addEventListener("click", function (e) {
+  edu.classList.toggle("tabact");
+  exp.classList.remove("tabact");
+  skill.classList.remove("tabact");
+});
+exp.addEventListener("click", function (e) {
+  exp.classList.toggle("tabact");
+  skill.classList.remove("tabact");
+  edu.classList.remove("tabact");
+});
+
+const more = document.querySelector("#more");
+const less = document.querySelector("#less");
+const see = document.querySelectorAll(".see");
+more.addEventListener("click", function (e) {
+  for (let se of see) {
+    se.style.display = "block";
+  }
+  more.style.display = "none";
+  less.style.display = "block";
+});
+less.addEventListener("click", function (e) {
+  for (let se of see) {
+    se.style.display = "none";
+  }
+  more.style.display = "block";
+  less.style.display = "none";
+});
